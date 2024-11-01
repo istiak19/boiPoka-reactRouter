e82541039a6bce2aedda4b41f50396a708d352c1
istiak19 <istiak15-14128@diu.edu.bd>
2024-11-01 08:58:16 +0600

    third commit

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -6,7 +6,7 @@ const Book = ({ book }) => {
     const {bookId, image, author, bookName, tags, rating, category } = book
     return (
         <Link to={`book/${bookId}`}>
-            <div className="card bg-base-100 w-96 shadow-xl p-6 border space-y-5">
+            <div className="card bg-base-100 shadow-xl p-6 border space-y-5">
                 <figure className='bg-blue-200 p-6 rounded-lg'>
                     <img className='h-40 w-36 rounded-lg'
                         src={image}
@@ -14,10 +14,10 @@ const Book = ({ book }) => {
                 </figure>
                 <div className='flex flex-row gap-8'>
                     {
-                        tags.map(tag => <button className="btn btn-xs text-[#23BE0A] font-medium">{tag}</button>)
+                        tags.map((tag,idx) => <button className="btn btn-xs text-[#23BE0A] font-medium" key={idx}>{tag}</button>)
                     }
                 </div>
-                <h2 className="card-title flex-grow">{bookName}</h2>
+                <h2 className="card-title">{bookName}</h2>
                 <p className='border-b-2 border-dotted pb-5 font-medium text-gray-600'>By: {author}</p>
                 <div className='flex justify-between'>
                     <p className='font-medium'>{category}</p>
--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -1,10 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetails = () => {
     const { bookId } = useParams()
+    const id = parseInt(bookId)
+    const data = useLoaderData()
+    const book = data.find(d => d.bookId === id)
+    const { bookId: currentBookId, image, review, author, yearOfPublishing, publisher, category, rating, totalPages, bookName, tags } = book
     return (
-        <div>
-            <h2>BookDetails {bookId}</h2>
+        <div className="hero">
+            <div className="hero-content flex-col lg:flex-row">
+                <img
+                    src={image}
+                    className="bg-base-200 p-16 rounded-lg" />
+                <div className="space-y-2">
+                    <h1 className="text-5xl font-bold">{bookName}</h1>
+                    <p className="border-b-2 pb-3">By: {author}</p>
+                    <p className="border-b-2 pb-3">{category}</p>
+                    <p className="py-6 text-justify"><span className="font-semibold">Review:</span> {review}</p>
+                    <p className="border-b-2 pb-3"><span className="font-bold mr-6">Tag: {
+                        tags.map((tag, idx) => <button key={idx} className="btn btn-xs text-[#23BE0A] mr-6">#{tag}</button>)
+                    }</span></p>
+                    <p>Number of Pages: <span className="font-bold">{totalPages}</span></p>
+                    <p>Publisher: <span className="font-bold">{publisher}</span></p>
+                    <p>Year of Publishing: <span className="font-bold">{yearOfPublishing}</span></p>
+                    <p>Rating: <span className="font-bold">{rating}</span></p>
+                    <button className="btn btn-outline mr-4 btn-accent">Read</button>
+                    <button className="btn bg-[#50B1C9]">Wishlist</button>
+                </div>
+            </div>
         </div>
     );
 };
