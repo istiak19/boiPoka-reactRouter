467fa2b114cad8e025c659610125f5067ac1d0d8
istiak19 <istiak15-14128@diu.edu.bd>
2024-11-02 08:20:21 +0600

    final commit

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -11,6 +11,7 @@ const ListedBooks = () => {
     const books = useLoaderData()
     const [reads, setReads] = useState([])
     const [wish, setWish] = useState([])
+    const [sort, setSort] = useState('')
     useEffect(() => {
         const storedReadList = getStoreReadList()
         const storedReadListIn = storedReadList.map(id => parseInt(id))
@@ -25,9 +26,32 @@ const ListedBooks = () => {
         setWish(wishList)
     }, [])
 
+    const handleSort = (sortType) => {
+        setSort(sortType)
+        if (sortType === 'Rating') {
+            const sortedReadList = [...reads].sort((a, b) => a.rating - b.rating)
+            setReads(sortedReadList)
+            const sortedWishListed = [...wish].sort((a, b) => a.rating - b.rating)
+            setWish(sortedWishListed)
+        }
+        else if (sortType === 'No Of Page') {
+            const sortedReadLists = [...reads].sort((a, b) => b.totalPages - a.totalPages)
+            setReads(sortedReadLists)
+            const sortedWishListes = [...wish].sort((a, b) => b.totalPages - a.totalPages)
+            setWish(sortedWishListes)
+        }
+
+    }
 
     return (
         <div>
+            <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn m-1">{sort ? `Sort By: ${sort}` : 'Sort By'}</div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li onClick={() => handleSort('Rating')}><a>Rating</a></li>
+                    <li onClick={() => handleSort('No Of Page')}><a>No Of Page</a></li>
+                </ul>
+            </div>
             <Tabs>
                 <TabList>
                     <Tab>Read Books</Tab>
